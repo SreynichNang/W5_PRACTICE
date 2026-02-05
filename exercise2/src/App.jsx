12a2b27366afb1fca8c0960275359335859a377d
Sreynich <srey45224@gmail.com>
2026-02-05 15:44:47 +0700

    done

---
+++ exercise2/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from './new/Header'
+import Main from './new/Main'
+import Footer from './new/Footer'
+
+const App = () => {
+
+  return (
+    <>
+      <Header/>
+      <Main/>
+      <Footer/>
+    </>
+  )
+}
+
+export default App
---
+++ exercise2/src/new/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () =>{
+  return(
+    <header class="block">
+      <h1>Welcome again !</h1>
+      <p></p>
+    </header>
+  );
+}
+export default Header
